af8006768dd395747b9bf1afc02883bec074dd4c
vahidrobati <vahid.robati@gmail.com>
2024-11-05 10:31:08 +0330

    Refactoring - remove duplicate interfaces

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,14 +1,14 @@
 import { GameQuery } from "../App";
 import { FetchResponse } from '../services/api-client';
-import { Genre } from "./useGenres";
 import apiClient from "../services/api-client";
 import { useQuery } from "@tanstack/react-query";
+import { Platform } from "./usePlatforms";
 
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
+// export interface Platform {
+//   id: number;
+//   name: string;
+//   slug: string;
+// }
 
 export interface Game {
   id: number;

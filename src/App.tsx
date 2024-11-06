bae96e868aff936a20c7302a4f8b3de486bff0d3
vahidrobati <vahid.robati@gmail.com>
2024-11-07 00:30:17 +0330

    refactoring - simplifying query objects

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,19 @@ import GenreList from "./components/GenreList";
 import NavBar from "./components/NavBar";
 import PlatformSelector from "./components/PlatformSelector";
 import SortSelector from "./components/SortSelector";
-import { Platform } from "./hooks/usePlatforms"
+import { Platform } from "./hooks/usePlatforms";
 import { Genre } from "./hooks/useGenres";
 
-
-export interface GameQuery { 
-  genre: Genre | null;
-  platform: Platform | null;
+export interface GameQuery {
+  genreId?: number;
+  platformId?: number;
   sortOrder: string;
   searchText: string;
 }
 
 function App() {
   const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
-  
+
   return (
     <Grid
       templateAreas={{
@@ -27,26 +26,43 @@ function App() {
         lg: `"nav nav" "aside main"`,
       }}
       templateColumns={{
-        base: '1fr',
-        lg: '250px 1fr'
+        base: "1fr",
+        lg: "250px 1fr",
       }}
     >
       <GridItem area="nav">
-        <NavBar onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })} />
+        <NavBar
+          onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })}
+        />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
-          <GenreList selectedGenre={gameQuery.genre} onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre})} />
+          <GenreList
+            selectedGenreId={gameQuery.genreId}
+            onSelectGenre={(genre) =>
+              setGameQuery({ ...gameQuery, genreId: genre.id })
+            }
+          />
         </GridItem>
       </Show>
       <GridItem area="main">
-        <Box paddingLeft={2}>
+        <Box paddingLeft={2}> 
           <GameHeading gameQuery={gameQuery} />
           <Flex marginBottom={5}>
             <Box marginRight={5}>
-              <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({ ...gameQuery, platform}) } />
+              <PlatformSelector
+                selectedPlatformId={gameQuery.platformId}
+                onSelectPlatform={(platform) =>
+                  setGameQuery({ ...gameQuery, platformId: platform.id })
+                }
+              />
             </Box>
-            <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({ ...gameQuery, sortOrder })} />
+            <SortSelector
+              sortOrder={gameQuery.sortOrder}
+              onSelectSortOrder={(sortOrder) =>
+                setGameQuery({ ...gameQuery, sortOrder })
+              }
+            />
           </Flex>
         </Box>
         <GameGrid gameQuery={gameQuery} />
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -13,9 +13,6 @@ export interface Game {
   rating_top: number;
 }
 
-interface PageQuery {
-  pageSize: number;
-}
 
 const apiClient = new APIClient<Game>('/games');
 
@@ -24,15 +21,16 @@ const useGames = (gameQuery: GameQuery) =>
   useInfiniteQuery<FetchResponse<Game> , Error>({
     queryKey: ["games", gameQuery],
     queryFn: ({ pageParam = 1 }) => apiClient.getAll({params: {
-      genres: gameQuery.genre?.id,
-      parent_platforms: gameQuery.platform?.id,
+      genres: gameQuery.genreId,
+      parent_platforms: gameQuery.platformId,
       ordering: gameQuery.sortOrder,
       search: gameQuery.searchText,
       page: pageParam
     },}),     
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ? allPages.length + 1: undefined;
-    }
+    },
+    staleTime : 24 * 60 * 60 * 1000 //24h
   });
 
 export default useGames;

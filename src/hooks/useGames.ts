68595d187e9ef1711f1aad3d4eb11925825d46be
vahidrobati <vahid.robati@gmail.com>
2024-11-07 14:45:02 +0330

    refactoring-symplify-time0-calculation

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,6 +3,7 @@ import { FetchResponse } from '../services/api-client';
 import APIClient from "../services/api-client";
 import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { Platform } from "./usePlatforms";
+import ms from "ms";
 
 export interface Game {
   id: number;
@@ -30,7 +31,7 @@ const useGames = (gameQuery: GameQuery) =>
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ? allPages.length + 1: undefined;
     },
-    staleTime : 24 * 60 * 60 * 1000 //24h
+    staleTime : ms('24h') //24h
   });
 
 export default useGames;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
 import APIClient from "../services/api-client";
-import { FetchResponse } from "../services/api-client";
+import ms from "ms";
 
 export interface Platform {
   id: number;
@@ -9,14 +9,14 @@ export interface Platform {
   slug: string;
 }
 
-const apiClient = new APIClient<Platform>('/platform/lists/parents');
+const apiClient = new APIClient<Platform>("/platform/lists/parents");
 
 const usePlatforms = () =>
   useQuery({
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 *1000, // 24h
-    //initialData: { count: platforms.length, results: platforms, next: null}
+    staleTime: ms("24h"), // 24h
+    initialData: { count: platforms.length, results: platforms, next: null },
   });
 
 export default usePlatforms;
